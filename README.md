a0fce3f8d1d6019e6db669a314e020a32c80b9ee
T.J. Corrigan <tjcorr@github.com>
2023-05-10 19:12:30 +0000

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # gha-unique-name-generator
-A simple GitHub Action to generate a unique string
+
+A simple GitHub Action to generate a unique name. 
+
+## Inputs
+
+### `separator`
+
+**Required** What separator to use. Default `-`.
+
+### `prefix`
+
+An optional prefix to add to the beginning of the name.
+
+### `suffix`
+
+An optional suffix to add to the end of the name.
+
+## Outputs
+
+### `name`
+
+The unique name output.
+
+## Example usage
+
+```
+uses: tjcorr/gha-unique-name-generator@v1
+id: generator
+with:
+  separator: '-'
+  prefix: 'pre'
+  suffix: 'post'
+
+name: test generator output
+run: echo "${{ steps.generator.outputs.name }}" // pre-red-fox-post
+```
---
+++ index.js
@@ -0,0 +1,85 @@
+const core = require('@actions/core')
+const { uniqueNamesGenerator, NumberDictionary } = require('unique-names-generator')
+
+// 11 colors
+const colors = [
+    'red','orange','yellow','blue','green','purple','pink','brown','gray','white','black'
+]
+
+// 45 animals
+const animals = [
+    'ant',
+    'bat',
+    'bee',
+    'bear',
+    'beaver',
+    'bird',
+    'bison',
+    'camel',
+    'cat',
+    'chicken',
+    'cow',
+    'crab',
+    'deer',
+    'dog',
+    'duck',
+    'eagle',
+    'elk',
+    'emu',
+    'falcon',
+    'fish',
+    'frog',
+    'fox',
+    'goat',
+    'horse',
+    'lion',
+    'lizard',
+    'moth',
+    'mouse',
+    'mule',
+    'owl',
+    'panda',
+    'pig',
+    'snail',
+    'snake',
+    'sheep',
+    'shrimp',
+    'spider',
+    'tiger',
+    'toad',
+    'tuna',
+    'walrus',
+    'wasp',
+    'whale',
+    'wolf',
+    'zebra',
+]
+
+try {
+    var prefix = core.getInput('prefix')
+    var suffix = core.getInput('prefix')
+    const separator = core.getInput('separator')
+
+    const numberDictionary = NumberDictionary.generate({ min: 1, max: 99 });
+
+    const config = {
+      dictionaries: [colors, animals, numberDictionary],
+      separator: '-',
+      style: 'lowercase'
+    };
+
+    if(prefix) {
+        prefix = prefix + separator
+    }
+
+    if(suffix) {
+        suffix = separator + suffix
+    }
+
+    const unique = uniqueNamesGenerator(config); 
+    const name = prefix + unique + suffix
+    console.log(`Unique name: ${name}`)
+    core.setOutput('name', name)
+} catch (error) {
+    core.setFailed(error.message)
+}
